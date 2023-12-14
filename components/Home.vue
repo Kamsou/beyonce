9f0ac3ddd67746bb51f2da9cf8420629d737e94e
Camille Coutens <camille.coutens@gmail.com>
2023-12-14 22:33:54 +0000

    initial commit

---
+++ components/Home.vue
@@ -0,0 +1,116 @@
+<template>
+  <section class="bg-[#090705] text-white font-regular">
+    <div
+      class="flex justify-between absolute w-full px-16 pt-9 text-[1.5vw] text-[#EBFF00] font-title"
+    >
+      <h1>Renaissance</h1>
+      <h2>Beyonce</h2>
+    </div>
+
+    <img
+      src="https://res.cloudinary.com/augalo/image/upload/v1702586088/Beyonc%C3%A9/beyonce-1_biekh2.jpg"
+      alt="Beyoncé sur son cheval en argent"
+      class="h-screen w-screen object-cover object-top"
+    />
+
+    <p
+      class="text-[2.500vw] text-[#EBFF00] font-title text-center transform -translate-y-[2rem]"
+    >
+      N’oubliez jamais de creer
+    </p>
+
+    <div class="px-[16.667vw] pt-[12.847vw]">
+      <div class="flex gap-[8.889vw] items-center">
+        <img
+          src="https://res.cloudinary.com/augalo/image/upload/v1702589686/Beyonc%C3%A9/beyonce-2_jqb8qu.jpg"
+          alt=""
+          class="w-[25.764vw]"
+        />
+
+        <div>
+          <p class="text-[1.111vw]">
+            Beyoncé Knowles-Carter, mondialement connue sous le nom de Beyoncé,
+            est une chanteuse, compositrice, productrice et actrice américaine,
+            née le 4 septembre 1981 à Houston, au Texas. Elle est reconnue pour
+            sa voix puissante, ses performances dynamiques et son influence
+            culturelle significative. Sa carrière a commencé dans les années
+            1990 avec le groupe R&B Destiny's Child, l'un des groupes féminins
+            les plus vendus de tous les temps. <br /><br />
+
+            Après le succès de Destiny's Child, Beyoncé a lancé sa carrière solo
+            en 2003 avec l'album "Dangerously in Love", qui a été un succès
+            commercial et critique, incluant des hits comme "Crazy in Love" et
+            "Baby Boy". Son talent et sa polyvalence lui ont permis de s'imposer
+            rapidement comme une des plus grandes artistes de sa génération.
+          </p>
+        </div>
+      </div>
+
+      <img
+        src="https://res.cloudinary.com/augalo/image/upload/v1702586088/Beyonc%C3%A9/beyonce-3_lh4asg.jpg"
+        alt=""
+        class="w-[69.167vw] mx-auto pt-[12.847vw]"
+      />
+
+      <div class="flex justify-end py-36">
+        <p class="w-[36.389vw] text-[1.111vw]">
+          Au cours de sa carrière, Beyoncé a sorti plusieurs albums acclamés,
+          tels que "B'Day", "I Am... Sasha Fierce", "4", "Beyoncé" et
+          "Lemonade". Chacun de ces albums a démontré son habileté à mélanger
+          différents genres musicaux et à aborder des thèmes comme l'amour,
+          l'empowerment des femmes, et les questions raciales et sociales.
+        </p>
+      </div>
+
+      <p
+        class="text-[2.500vw] text-[#EBFF00] font-title text-center mb-[12.361vw]"
+      >
+        La confiance en soi
+      </p>
+
+      <div>
+        <img
+          src="https://res.cloudinary.com/augalo/image/upload/v1702586088/Beyonc%C3%A9/beyonce-4_es0psd.jpg"
+          alt=""
+        />
+
+        <div class="py-[10.347vw]">
+          <p class="text-[1.111vw] w-[36.389vw] mx-auto">
+            En plus de sa carrière musicale, Beyoncé a également joué dans
+            plusieurs films, y compris "Austin Powers in Goldmember",
+            "Dreamgirls" et "The Lion King", où elle a prêté sa voix à Nala.
+            <br /><br />
+
+            Beyoncé est également connue pour son travail humanitaire et son
+            activisme. Elle a contribué à diverses causes, notamment la
+            sensibilisation aux droits des femmes, l'éducation et la lutte
+            contre la faim.
+          </p>
+        </div>
+
+        <div class="flex justify-end">
+          <img
+            src="https://res.cloudinary.com/augalo/image/upload/v1702586088/Beyonc%C3%A9/beyonce-5_kuge8q.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <p
+        class="text-[2.500vw] text-[#EBFF00] font-title text-center mt-[12.361vw]"
+      >
+        Le travail, la discipline
+      </p>
+    </div>
+  </section>
+
+  <footer
+    class="bg-[#090705] text-white pt-64 pb-14 flex justify-end px-[16.667vw]"
+  >
+    <div class="flex justify-between w-[205px]">
+      <span>LINK</span>
+      <span>LINK</span>
+      <span>LINK</span>
+    </div>
+  </footer>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        regular: ["Cabinet Grotesk", "sans-serif"],
+        title: ["OTR Type", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
